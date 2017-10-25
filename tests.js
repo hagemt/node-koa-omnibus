7c71e27895db3a46bd7fba238eabf6031eb14f14
Tor Hagemann <tohagema@cisco.com>
2017-10-25 04:23:26 -0700

    tests.js: add single formalized (smoke) test

--- tests.js
+++ tests.js
@@ -1,10 +1,28 @@
 /* eslint-env mocha, node */
-const assert = require('assert')
+const HTTP = require('http')
 
-const omnibus = require('.')
+const supertest = require('supertest')
+
+const omnibus = require('.') // index.js
 
 describe('omnibus', () => {
-	it('is a middleware factory of epic win', () => {
-		assert(omnibus)
+
+	const test = {}
+
+	before(() => {
+		const application = omnibus.createApplication()
+		const server = HTTP.createServer() // supertest'd
+		server.on('request', application.callback())
+		Object.assign(test, { server })
 	})
+
+	it('is a middleware factory of epic win', () => {
+		return supertest(test.server).get('/').expect(404)
+	})
+
+	after(() => {
+		test.server.close()
+		delete test.server
+	})
+
 })
